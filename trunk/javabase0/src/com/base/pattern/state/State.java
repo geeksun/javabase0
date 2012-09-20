fa441f9a4600ff1dd5aa869a08faf3eadddc786b
jzq1999 <jzq1999@db87251f-2b0d-09c2-db99-1dfdd91474f3>
2012-09-20 00:07:05 +0000

    状态模式

---
+++ trunk/javabase0/src/com/base/pattern/state/State.java
@@ -0,0 +1,11 @@
+package com.base.pattern.state;
+
+/**
+ * Description ×´Ì¬½Ó¿Ú
+ * @author Administrator
+ * 2012-9-19
+ */
+public interface State {
+	
+	void handle();
+}
---
+++ trunk/javabase0/src/com/base/pattern/state/StateB.java
@@ -0,0 +1,11 @@
+package com.base.pattern.state;
+
+public class StateB implements State {
+
+	@Override
+	public void handle() {
+		// TODO Auto-generated method stub
+		System.out.println("B״̬");
+	}
+
+}
